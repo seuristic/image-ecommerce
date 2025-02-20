b78c9054dd47a5978c75fac87e51ec9c3f00bf28
Shahanwaz <mohdshahanwaz55@gmail.com>
2025-02-20 16:09:35 +0530

    fix: mailtrap config -- host

--- src/app/api/webhook/razorpay/route.ts
+++ src/app/api/webhook/razorpay/route.ts
@@ -38,8 +38,8 @@ export async function POST(req: NextRequest) {
 
       if (order) {
         const transporter = nodemailer.createTransport({
-          service: 'sandbox.smtp.mailtrap.io',
-          port: 25,
+          host: 'sandbox.smtp.mailtrap.io',
+          port: 2525,
           auth: {
             user: process.env.MAILTRAP_USER,
             pass: process.env.MAILTRAP_PASS
